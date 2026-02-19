0d649251921d6f5e1ce20e1d5a72bcebf9117702
vinod kumar <vinu041722@gmail.com>
2026-02-19 20:55:42 +0530

    Day 1 - Python fundamentals (variables, data types, type conversion, operators)

---
+++ python-basic-to-advance/day_1.py
@@ -0,0 +1,16 @@
+# declaring the variables
+name = 'vinod'
+age = 25
+company = "Soft Tech"
+designation = "Technology Analyst"
+salary = 50000
+is_permanent_employee = True
+lpa = 8.5
+
+
+# Data types
+print(type(name))   # string
+print(type(age))    # integer
+print(type(lpa))    #  float
+print(type(is_permanent_employee)) # boolean
+
