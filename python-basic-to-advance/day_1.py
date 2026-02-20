7b4fd396b6c1840acf8499792691a4100c2a4ae1
vinod kumar <vinu041722@gmail.com>
2026-02-20 19:18:28 +0530

    day_1 : Added type conversion examples

--- python-basic-to-advance/day_1.py
+++ python-basic-to-advance/day_1.py
@@ -14,3 +14,19 @@ print(type(age))    # integer
 print(type(lpa))    #  float
 print(type(is_permanent_employee)) # boolean
 
+# type conversion
+lpa = int(lpa)
+print(type(lpa)) # integer
+
+salary = float(salary)
+print(type(salary))
+
+age = str(age)
+print(type(age))
+
+name = int(name)
+print(type(name)) # incase try to convert the string into integer it will give a ValueError
+
+company = int(company)
+print(type(company)) # incase try to convert the string into integer it will give a ValueError
+
