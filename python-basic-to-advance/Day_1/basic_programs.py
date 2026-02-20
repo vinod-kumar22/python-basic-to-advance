755cede7a05dacb3ce8b3bdbbd78480dbd821706
vinod kumar <vinu041722@gmail.com>
2026-02-20 22:05:40 +0530

    Day 1 - Implemented 10 practice programs

---
+++ python-basic-to-advance/Day_1/basic_programs.py
@@ -0,0 +1,82 @@
+# Calculator
+a = 20
+b = 30
+
+addition = a + b
+subtraction = b - a
+multiplication = a * b
+division = a / b
+print(f'addition : {addition}, subtraction : {subtraction}, multiplication : {multiplication}, division: {multiplication}' ,sep=', ')
+
+# Temperature converter
+celsius = 49.5
+
+fahrenheit = (celsius * 9 / 5) + 32
+print(f'temperature in celsius : {celsius}, temperature in fahrenheit : {fahrenheit}' ,sep=', ')
+
+# Area of circle
+import  math
+radius = 5
+area_of_circle = math.pi * radius ** 2
+print(f'area of circle : {area_of_circle}')
+
+# Area of rectangle
+
+length = 20
+width = 6
+
+area_of_rectangle = length * width
+print(f'area of rectangle : {area_of_rectangle}')
+
+# Simple interest
+
+principal =800000
+time = 5
+rate = 16
+
+simple_interest = (principal * time * rate) / 12
+print(f'simple interest : {simple_interest}')
+
+# Swap two numbers
+i = 10
+j = 20
+
+print(f'before swaping : {i} and {j}')
+i,j = j,i
+print(f'after swaping : {i} and {j}')
+
+# Even or Odd checker
+num = 40
+if num % 2 == 0:
+    print(f'{num} is a even number')
+else:
+    print(f'{num} is a odd number')
+
+# Largest between 2 numbers
+
+x = 30
+y = 10
+
+if x > y:
+    print(f'{x} is larger number than {y}')
+else:
+    print(f'{y} is larger number than {x}')
+
+
+# Square and Cube
+
+number = 10
+square = number ** 2
+cube = number ** 3
+
+print(f'square of {number} is {square} and cube of {number} is {cube}')
+
+
+# BMI Calculator
+
+weight = 72
+height = 1.6
+
+body_mass_index = weight / (height* height)
+
+print(f'body mass index is {body_mass_index}')
